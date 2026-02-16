2d7a38e2c6c08d98105ec706bbb3850344378fe1
WINDERMAN DANOBIS MARIN GARCIA <windermanmarin1157959@correo.itm.edu.co>
2026-02-16 17:43:35 -0500

    process shipping

---
+++ Shipping/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var weight = ConsoleExtension.GetFloat("Peso de a mercancia (kg).............: ");
+    var value = ConsoleExtension.GetDecimal("Valor de la mercancia (COP)........: ");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
