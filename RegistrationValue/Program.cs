3fb9a3ccf31e63afe3d664d14e3aadfb71dd2b8f
WINDERMAN DANOBIS MARIN GARCIA <windermanmarin1157959@correo.itm.edu.co>
2026-02-16 16:07:06 -0500

    Complete Regitation Value

---
+++ RegistrationValue/Program.cs
@@ -0,0 +1,63 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };    
+
+do
+{
+    var credits = ConsoleExtension.GetInt        ("Número de créditos...............: ");
+    var creditValue = ConsoleExtension.GetDecimal("Valor del crédito................: ");
+    var stratum = ConsoleExtension.GetInt        ("Estrato socioeconómico (1-6).....: ");
+
+    var registrationValue = CalculateRegistrationValue(credits, creditValue, stratum);
+    var subsidy = CalculateSubsidy(stratum); 
+
+    Console.WriteLine($"Valor de matrícula...............: {registrationValue,20:C2}");
+    Console.WriteLine($"Valor del subsidio...............: {subsidy,20:C2}");
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
+
+decimal CalculateSubsidy(int stratum)
+{
+    if (stratum == 1)
+    {
+        return 200000m;
+    }
+    if (stratum == 2)
+    {
+        return 100000m;
+    }
+   return 0m;
+}
+decimal CalculateRegistrationValue(int credits, decimal creditValue, int stratum)
+{
+    decimal value;
+    if (credits <= 20)
+    {
+        value = credits * creditValue;
+    }
+    else
+    {
+        value = 20 * creditValue + (credits - 20) * creditValue * 2;
+    }
+        if (stratum == 1)
+    {
+        return value * 0.2m;
+    }
+    if (stratum == 2)
+    {
+        return value * 0.5m;
+    }
+    if (stratum == 3)
+    {
+        return value * 0.7m;
+    }
+    return value;
+}
+
+
