2f85b2cdb1f06919d08cafd0cc48ed4cbb2700e0
WINDERMAN DANOBIS MARIN GARCIA <windermanmarin1157959@correo.itm.edu.co>
2026-02-18 10:52:32 -0500

    Quadratic Ecuation complete

---
+++ QuadraticEcuation/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+   var a = ConsoleExtension.GetDouble("Ingrese el valor de a: ");
+   var b = ConsoleExtension.GetDouble("Ingrese el valor de b: ");
+   var c = ConsoleExtension.GetDouble("Ingrese el valor de c: ");
+   var solution = QuadraticEcuation(a, b, c);
+   Console.WriteLine($"x1 = {solution.X1}");
+   Console.WriteLine($"x2 = {solution.X2}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over.");
+
+
+QuadraticEcuationSolution QuadraticEcuation(double a, double b, double c)
+{
+   return new QuadraticEcuationSolution
+    {
+        X1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a),
+        X2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a)
+    };
+}
+public class QuadraticEcuationSolution 
+{
+    public double X1 { get; set; }
+
+    public double X2 { get; set; }
+}
