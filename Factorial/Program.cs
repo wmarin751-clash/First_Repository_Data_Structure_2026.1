3c750a5295f161cf8381ad0e62b25a81d24a16a1
WINDERMAN DANOBIS MARIN GARCIA <windermanmarin1157959@correo.itm.edu.co>
2026-02-17 20:11:30 -0500

    factorial complete

---
+++ Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingrese un número entero: ");
+    var factorial = MyMath.Factorial(n);
+
+    Console.WriteLine($"El factorial de {n} es: {factorial}");
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over."); ;
