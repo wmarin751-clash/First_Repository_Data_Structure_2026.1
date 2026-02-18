f5e2fc0d862b61ce2edc5e5ffbc8afdc1478f759
WINDERMAN DANOBIS MARIN GARCIA <windermanmarin1157959@correo.itm.edu.co>
2026-02-17 21:02:42 -0500

    taylor modified complete

---
+++ TaylorModified/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("cuantos terminos desea: ");
+    var x = ConsoleExtension.GetDouble("Ingrese el valor de x:  ");
+    var taylor = TaylorModified(n, x);
+    Console.WriteLine($"f ({x}) = {taylor:N5}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over.");
+double TaylorModified(int n, double x)
+{
+    double sum = 0;
+    int signo = 1;
+    for (int i = 0; i < n; i++)
+    {
+        sum += Math.Pow(x, i) / MyMath.Factorial(i) * signo;
+        signo *= -1;
+    }
+    return sum;
+}
